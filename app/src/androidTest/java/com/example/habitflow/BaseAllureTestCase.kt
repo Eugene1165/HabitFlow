d72cdd83b18ae7d0d16df7c2bb1a463c3b607616
Eugene123 <alwayswannafly34@gmail.com>
2026-03-27 12:45:31 +0300

    feat:фикс job для ui тестов,генерация отчета

--- app/src/androidTest/java/com/example/habitflow/BaseAllureTestCase.kt
+++ app/src/androidTest/java/com/example/habitflow/BaseAllureTestCase.kt
@@ -1,14 +1,21 @@
 package com.example.habitflow
 
+import androidx.test.rule.GrantPermissionRule
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import com.kaspersky.components.alluresupport.addAllureSupport
+import org.junit.Rule
 
 open class BaseAllureTestCase: TestCase(
     kaspressoBuilder = Kaspresso.Builder.simple().apply{
         addAllureSupport()
     }
 ) {
+    //Это JUnit Rule, который выдаёт разрешение до запуска теста, без показа диалога:
+    @get:Rule
+    val notificationPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        android.Manifest.permission.POST_NOTIFICATIONS
+    )
 
 }
 
