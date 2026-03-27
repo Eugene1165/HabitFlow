a6605cafb7cce199e278eb47b847d859c384ce22
Eugene123 <alwayswannafly34@gmail.com>
2026-03-27 13:02:50 +0300

    feat:фикс job для ui тестов

--- app/src/androidTest/java/com/example/habitflow/BaseAllureTestCase.kt
+++ app/src/androidTest/java/com/example/habitflow/BaseAllureTestCase.kt
@@ -1,21 +1,24 @@
 package com.example.habitflow
 
+import android.os.Build
 import androidx.test.rule.GrantPermissionRule
+import com.kaspersky.components.alluresupport.addAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import com.kaspersky.components.alluresupport.addAllureSupport
 import org.junit.Rule
+import org.junit.rules.TestRule
 
-open class BaseAllureTestCase: TestCase(
-    kaspressoBuilder = Kaspresso.Builder.simple().apply{
+open class BaseAllureTestCase : TestCase(
+    kaspressoBuilder = Kaspresso.Builder.simple().apply {
         addAllureSupport()
     }
 ) {
-    //Это JUnit Rule, который выдаёт разрешение до запуска теста, без показа диалога:
     @get:Rule
-    val notificationPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        android.Manifest.permission.POST_NOTIFICATIONS
-    )
-
+    val notificationPermissionRule: TestRule =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            GrantPermissionRule.grant(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            TestRule { base, _ -> base }
+        }
 }
 
